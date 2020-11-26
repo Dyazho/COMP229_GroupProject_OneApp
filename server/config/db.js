30841a7f31bf789c263f826895ff4de4930b484f
Munther Al Shukair <monthershqair@gmail.com>
2020-11-26 16:18:14 -0500

    Update URI to work with mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,15 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/survey_list",
+    //"URI": "mongodb://localhost/survey_list",
+    "URI": "mongodb+srv://munther:lJZjykUTEP61n3Sw@mongodbserver.gsgfm.mongodb.net/survey_list?retryWrites=true&w=majority"
     /* will create an opject if we are going to convert to ANGULAR */
     //"Secret": "SomeSecert"
 }
 
 // npm install mongoose --save
 /*
-
-
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:<password>@survey.gsgfm.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://<username>:<password>@mongodbserver.gsgfm.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
@@ -19,13 +18,17 @@ client.connect(err => {
 });
 
 });
-the password is: Wjn2FsisojpG2rVO
+
+});
+the password is: lJZjykUTEP61n3Sw
 
 */
 
 /*
 https://github.com/Munther-Al-Shukair/COMP229-F2020-Team-Progect.git
+https://github.com/Munther-Al-Shukair/COMP229-F2020-Team-Project.git
 */
 /* private
 https://github.com/Munther-Al-Shukair/COMP229-F2020-Team-Project-OneApp.git
+
 */
